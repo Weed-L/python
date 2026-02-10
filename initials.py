856845596b9e93579957fcffe018817e2faf1f84
Weed-L <lweed5@fingerlakes.edu>
2026-02-10 01:24:43 +0000

    Added rectangle and initials files for labs

---
+++ initials.py
@@ -0,0 +1,7 @@
+#Lorenzo Weed
+print("LLLL         WWW    WWW")
+print("LLLL         WWW    WWW")
+print("LLLL         WWW    WWW")
+print("LLLL         WWW WW WWW")
+print("LLLL         WWW WW WWW")
+print("LLLLLLLLLL    WWW  WWW")
---
+++ rectangle.py
@@ -0,0 +1,5 @@
+#Lorenzo Weed
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
